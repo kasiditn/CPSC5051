47f7bc5a7a7e02f6dc4cffbb44a616d6f663520d
Kasidit Nusitchaiyakan <kasiditnusitchaiyakan@hotmail.com>
2019-05-14 17:36:47 -0700

    removed email and updated contact

--- LifeSaverSite/Controllers/HomeController.cs
+++ LifeSaverSite/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace LifeSaverSite.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Clinic Contact";
 
             return View();
         }
