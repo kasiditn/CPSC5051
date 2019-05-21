5b07d773cbba657c03e3e0caa38edb039eda3079
Kasidit Nusitchaiyakan <kasiditnusitchaiyakan@hotmail.com>
2019-05-21 15:14:13 -0700

    Updated home controller to inlcude image and report

--- LifeSaverSite/Controllers/HomeController.cs
+++ LifeSaverSite/Controllers/HomeController.cs
@@ -21,41 +21,53 @@ namespace HW1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your About Page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your Contact Page.";
 
             return View();
         }
 
         public ActionResult Login()
         {
-            ViewBag.Message = "Your login page.";
+            ViewBag.Message = "Your Login Page.";
 
             return View();
         }
         public ActionResult Compare()
         {
-            ViewBag.Message = "Your login page.";
+            ViewBag.Message = "Your Compare page.";
 
             return View();
         }
         public ActionResult SiteMap()
         {
-            ViewBag.Message = "Your login page.";
+            ViewBag.Message = "Your SiteMap page.";
 
             return View();
         }
         public ActionResult Dashboard()
         {
-            ViewBag.Message = "Your login page.";
+            ViewBag.Message = "Your Dashboard page.";
 
             return View();
         }
+        public ActionResult Report()
+        {
+            ViewBag.Message = "Your Report page.";
+
+            return View();
+        }
+        public ActionResult Image()
+           {
+            ViewBag.Message = "Your Image page.";
+
+            return View();
     }
+}
 }
